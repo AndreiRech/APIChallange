6dd3850546b0699c945edeeb936650828fcf389c
AndreiRech <andreirech22@outlook.com>
2025-08-13 15:28:01 -0300

    Initial Commit

---
+++ APIChallange/APIChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIChallangeApp.swift
+//  APIChallange
+//
+//  Created by Andrei Rech on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct APIChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIChallange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APIChallange
+//
+//  Created by Andrei Rech on 13/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
