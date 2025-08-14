cc6977b5df2845fc0b9d4f4c4a914826c22d6860
AndreiRech <andreirech22@outlook.com>
2025-08-14 17:33:52 -0300

    feat: changes

--- APIChallange/Views/Categories.swift
+++ APIChallange/Views/Categories.swift
@@ -8,47 +8,35 @@
 import SwiftUI
 
 struct Categories: View {
-    let viewModel: ProductViewModel
+    let viewModel: CategoryViewModel
     
-    @State var searchText = ""
-    
-    var filteredCategories: [ProductCategory] {
-        if searchText.isEmpty {
-            return ProductCategory.allCases
-        } else {
-            return ProductCategory.allCases.filter { $0.rawValue.lowercased().contains(searchText.lowercased()) }
-        }
-    }
+    @State var searchText: String = ""
     
     var body: some View {
         NavigationStack {
-            if viewModel.isLoading {
-                ProgressView()
-            } else {
-                VStack(spacing: 16) {
-                    ScrollView(.horizontal) {
-                        HStack(alignment: .center) {
-                            ForEach(filteredCategories) { category in
-                                VStack(spacing: 8) {
-                                    DefaultImage(imageName: category.imageName)
-                                    
-                                    Text(category.rawValue)
-                                        .font(.subheadline)
-                                }
-                                .frame(width: 84, height: 112)
+            VStack(spacing: 16) {
+                ScrollView(.horizontal) {
+                    HStack(alignment: .center) {
+                        ForEach(viewModel.filteredCategories(searchText: searchText)) { category in
+                            VStack(spacing: 8) {
+                                DefaultImage(imageName: category.imageName)
+                                
+                                Text(category.rawValue)
+                                    .font(.subheadline)
                             }
+                            .frame(width: 84, height: 112)
                         }
                     }
-                    .padding(.horizontal, 16)
-                    .scrollIndicators(.hidden)
-                    
-                    List(filteredCategories) { category in
-                        CategoryView(category: category)
-                    }
-                    .scrollIndicators(.hidden)
-                    .listStyle(.inset)
-                    .searchable(text: $searchText, prompt: "Search")
                 }
+                .padding(.horizontal, 16)
+                .scrollIndicators(.hidden)
+                
+                List(viewModel.filteredCategories(searchText: searchText)) { category in
+                    CategoryView(category: category)
+                }
+                .scrollIndicators(.hidden)
+                .listStyle(.inset)
+                .searchable(text: $searchText, prompt: "Search")
             }
         }
         .navigationTitle("Categories")
